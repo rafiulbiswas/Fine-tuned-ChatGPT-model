889837a36425c6f89dd2989a77c47d621be0f9bf
rafiulbiswas <rafiulbiswas@gmail.com>
2023-09-09 12:09:54 +0300

    Update script.py

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ def open_file(file_path):
 def save_file(file_path,content):
         with open(file_path, 'a', encoding='utf-8') as outfile:
              outfile.write(content)
-openai.api_key="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"
+openai.api_key="OPENAI API KEY"
 
 with open("output1.jsonl","rb") as file:
     response=openai.File.create(
